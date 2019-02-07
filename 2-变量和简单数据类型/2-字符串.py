51004be2b58bbc8a260613cd89e51c215ce9fd47
王焱辉 <wyh_19@163.com>
2019-02-07 14:53:51 +0800

    练习

--- 2-变量和简单数据类型/2-字符串.py
+++ 2-变量和简单数据类型/2-字符串.py
@@ -14,6 +14,7 @@ print(name.upper()) #输出 ADA LOVELACE
 # lower函数，使字符串全部小写
 print(name.lower()) #输出 ada lovelace
 
+
 # 拼接字符串，使用+
 first_name = "ada"
 last_name = "lovelace"
@@ -21,3 +22,16 @@ full_name = first_name + " " + last_name
 print(full_name) #输出 ada lovelace
 message = "Hello, " + full_name.title() + "!" 
 print(message) #输出 Hello, Ada Lovelace!
+
+#使用\t增加缩进，使用\n换行
+print('\tHello, \nWorld!')
+
+# 删除首尾的空白字符:strip函数
+msg = '  Hello World!  '
+print(msg.strip())
+# 只去除头部的空白
+print(msg.lstrip())
+# 只去除尾部的空白
+print(msg.rstrip())
+# 注意1：strip前面的l表示left,r表示right;也就是分别代表头与尾
+# 注意2：到目前演示位置，函数并没有改变原字符串，而是得到一个新的字符串，因此msg依然是'  Hello World!  '
