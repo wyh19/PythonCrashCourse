58dd021e374ab0f640fb313d2fbb8dc4b065d356
王焱辉 <wyh_19@163.com>
2019-02-08 19:36:56 +0800

    练习

--- 6-字典/1-介绍字典.py
+++ 6-字典/1-介绍字典.py
@@ -5,3 +5,15 @@ print(info)
 
 # 使用[key]访问字典中的值
 print(info['name'])
+
+# 添加或修改信息，已存在的信息则修改，不存在则添加
+info['gender'] = '男'
+print(info)
+info['name'] = 'wyh19'
+print(info)
+
+# 删除键值对，使用del语句
+del info['gender']
+print(info) # 删除了性别信息
+
+
