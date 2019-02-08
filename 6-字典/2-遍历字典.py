ee5694ad2f120258fd0018f5fe22396b7873c34b
王焱辉 <wyh_19@163.com>
2019-02-08 20:00:10 +0800

    练习

---
+++ 6-字典/2-遍历字典.py
@@ -0,0 +1,27 @@
+user_0 = {
+    'username': 'efermi', 'first': 'enrico', 'last': 'fermi',
+}
+
+# 字典通过items方法，得到一个列表
+print('---字典转换成列表---')
+print(user_0.items())
+# 遍历字典列表
+for key, value in user_0.items():
+    print("Key: " + key)
+    print("Value: " + value)
+
+# 通过keys获得key列表
+print('---通过keys获得key列表---')
+print(user_0.keys())
+# 遍历字典中所有的键
+for key in user_0.keys():
+    print(key.upper())
+
+# 通过values获得value列表
+print('---通过values获得value列表---')
+print(user_0.values())
+# 遍历字典中所有的值
+for value in user_0.values():
+    print(value.upper())
+
+
