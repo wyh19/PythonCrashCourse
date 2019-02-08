eff7c2eb49f676296325e66febb4280b535add1b
王焱辉 <wyh_19@163.com>
2019-02-08 16:50:30 +0800

    完善

--- 5-if语句/5-if语句.py
+++ 5-if语句/5-if语句.py
@@ -17,9 +17,24 @@ else:
 
 # if-elif-else演示
 age = 12
-if age<4:
+print('---if-elif-else演示---')
+if age < 4:
     print("Your admission cost is $0.")
 elif age < 18:
     print("Your admission cost is $5.")
 else:
     print("Your admission cost is $10.")
+
+# 注意 elif可以使用多个，主要看判断条件的划分
+# else 可以省略，如果不需要的话
+print('---演示多个elif以及else省略---')
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+elif age >= 65:
+    price = 3
+print("Your admission cost is $" + str(price) + ".")
