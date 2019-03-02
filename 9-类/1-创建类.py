8f9f5d815db6a3a0b4ea16abdfe5ea7ed6fd858b
王焱辉 <wyh_19@163.com>
2019-03-02 22:01:33 +0800

    练习

---
+++ 9-类/1-创建类.py
@@ -0,0 +1,11 @@
+class Dog():
+    """依次模拟小狗的简单尝试"""
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        print(self.name.title()+" is now sitting")
+
+    def roll_over(self):
+        print(self.name.title()+' rolled over')
