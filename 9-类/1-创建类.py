5b1886a16778a099d1c46d5ee04dd36f11fc4b08
王焱辉 <wyh_19@163.com>
2019-03-25 16:21:43 +0800

    优化

--- 9-类/1-创建类.py
+++ 9-类/1-创建类.py
@@ -1,11 +1,14 @@
 class Dog():
     """依次模拟小狗的简单尝试"""
-    def __init__(self,name,age):
+
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-    
+
     def sit(self):
         print(self.name.title()+" is now sitting")
 
     def roll_over(self):
         print(self.name.title()+' rolled over')
+
+my_dog = Dog('willie',6)
